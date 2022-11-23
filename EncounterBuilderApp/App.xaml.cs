f9b37d38005b2a7465b905050e05cec70c5f4aa7
zsui0 <zsoltplezer@gmail.com>
2022-11-23 11:31:47 +0100

    Add project files.

---
+++ EncounterBuilderApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EncounterBuilderApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ EncounterBuilderApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EncounterBuilderApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
